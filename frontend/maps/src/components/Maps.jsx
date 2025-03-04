e322a155da1b49cb59511f76b15c9b6303d6b1cf
saikiranyad <saisaikiran7890@gmail.com>
2025-03-04 18:47:01 +0530

    linkk

--- frontend/maps/src/components/Maps.jsx
+++ frontend/maps/src/components/Maps.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import io from 'socket.io-client';
 
