811ded69598e16e6c1507e670fe8cdcc3dfb6c50
saikiranyad <saisaikiran7890@gmail.com>
2025-03-04 18:43:28 +0530

    link changed

--- frontend/maps/src/components/Maps.jsx
+++ frontend/maps/src/components/Maps.jsx
@@ -3,7 +3,8 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+// const socket = io('http://localhost:5000');
+const socket = io('https://map-functionality.onrender.com');
 
 const Maps = () => {
     const [locations, setLocations] = useState({});
